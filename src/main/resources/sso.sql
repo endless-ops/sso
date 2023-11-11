600e6bf65194d21b93ef135542415ee8567101d5
endless-ops <test_endless@126.com>
2023-11-11 23:06:06 +0800

    新建用于存储用户信息的数据库脚本文件

---
+++ src/main/resources/sso.sql
@@ -0,0 +1,10 @@
+-- tutorial 上传到Github时，创建的仓库名为 tutorial-xxxx。为的是用于区分是否用于学习的教程
+
+-- 用于单点登录的数据库
+create database if not exists sso;
+
+-- 使用数据库 sso
+use sso;
+
+-- --------------------------------------------------------
+-- 创建表
